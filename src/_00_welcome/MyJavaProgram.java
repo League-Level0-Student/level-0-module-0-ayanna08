14fcbc00bcfef938d23d7a41a419a749bb8823f4
League Student <league@iMac-28.attlocal.net>
2019-12-01 14:59:13 -0800

    done

--- src/_00_welcome/MyJavaProgram.java
+++ src/_00_welcome/MyJavaProgram.java
@@ -7,6 +7,15 @@ public class MyJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+		Robot ayanna = new Robot();
+		ayanna.setSpeed(5);
+		ayanna.setRandomPenColor();
+		ayanna.penDown();
+		for (int i = 0; i < 4; i++) {
+			ayanna.move(100);
+			ayanna.turn(90);
+		}
+		
 	
 		
 		
